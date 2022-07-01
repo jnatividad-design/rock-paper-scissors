3a8c8553658e71998fe4f767d94794777ce21a5d
jnatividad-design <joshuamnatividad@yahoo.com>
2022-07-01 15:29:50 -0400

    Add player function to external.js

--- external.js
+++ external.js
@@ -1,14 +1,18 @@
 
 //Computer picks random number to pick rock,paper,scissors 
 let computerChoice = Math.floor(Math.random()*3);
-if ((computerChoice) == 0) {computerChoice=('Rock');
+if ((computerChoice) == 0) {computerChoice=('rock');
 }
-else if ((computerChoice) == 1) {computerChoice=('Paper');
+else if ((computerChoice) == 1) {computerChoice=('paper');
 }
-else {computerChoice =('Scissors');
+else {computerChoice =('scissors');
 }
 
 console.log (computerChoice)
 
 //Player selects between rock, paper, scissors
-let playerSelection= prompt ('Rock, Paper, or Scissors?');
+let playerSelection= prompt ('Rock, Paper, or Scissors?').toLowerCase(); 
+
+if ((playerSelection) !== 'rock' || 'paper' || 'scissors'); {
+    prompt ("Incorrect Input! Try again (Rock,Paper, or Scissors)")
+}
