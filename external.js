35091cb84aa74c632c03d7f87c70037c479215e0
jnatividad-design <joshuamnatividad@yahoo.com>
2022-07-01 22:52:04 -0400

    Add game function to external.js

--- external.js
+++ external.js
@@ -1,4 +1,5 @@
 
+
 //Computer picks random number to pick rock,paper,scissors 
 let computerChoice = Math.floor(Math.random()*3);
 if ((computerChoice) == 0) {computerChoice=('rock');
@@ -8,11 +9,52 @@ else if ((computerChoice) == 1) {computerChoice=('paper');
 else {computerChoice =('scissors');
 }
 
-console.log (computerChoice)
-
-//Player selects between rock, paper, scissors
-let playerSelection= prompt ('Rock, Paper, or Scissors?').toLowerCase(); 
-
-if ((playerSelection) !== 'rock' || 'paper' || 'scissors'); {
-    prompt ("Incorrect Input! Try again (Rock,Paper, or Scissors)")
+//Function to determine winner
+function playRound (playerSelection, computerChoice) {
+    if (playerSelection == computerChoice) {
+        return "Its a Tie!"
+    }
+    if (playerSelection == "rock") {
+        if (computerChoice == "paper") {
+            computerCount++;
+            return "You Lose! Paper Beats Rock";
+        }
+        else {
+            userCount++;
+            return "You Win!"
+        }
+    }
+    if (playerSelection == "paper") {
+        if (computerChoice == "scissors") {
+            computerCount++;
+            return "You Lose! Scissors beats Paper"
+        }
+        else {
+            userCount++;
+            return "You Win!"
+        }
+    }
+    if (playerSelection == "scissors") {
+        if (computerChoice == "rock") {
+            computerCount++;
+            return "You Lose! Rock beats Scissors"
+        }
+        else {
+            userCount++;
+            return "You Win!"
+        }
+    }
 }
+
+//Loop to have rounds in the game 
+let userCount = parseInt(0);
+let computerCount = parseInt(0);
+function game () {
+    for (let i = 0; i < 5; i++) {
+let playerSelection = prompt ('Rock, Paper, or Scissors?').toLowerCase(); 
+    console.log (playRound(playerSelection, computerChoice));
+    console.log ("your score =" + userCount);
+    console.log ("Computer's score" + computerCount);
+     } 
+    }
+console.log (game())
